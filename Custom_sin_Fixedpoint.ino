757087102a8ff4e5e20529ab13fa560d9969f2c1
jonas Jørgensen <jlja18@student.aau.dk>
2020-03-02 08:58:40 +0100

    Added beerware license

--- Custom_sin_Fixedpoint.ino
+++ Custom_sin_Fixedpoint.ino
@@ -1,6 +1,13 @@
 #include "math.h" 
 
-
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" 
+ * jlja18@student.aau.dk wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Jonas Jørgensen
+ * ----------------------------------------------------------------------------
+ */
 
 
 
